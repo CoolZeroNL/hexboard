88292538e7e5e47a1e29968fc2c92dfeed4add48
CoolZeroNL <info@local-events.nl>
2019-03-29 21:33:13 +0100

    Auto Add: 29-03-2019 21:32:53 [skip ci]

--- scripts/post_image.manual.js
+++ scripts/post_image.manual.js
@@ -1,6 +1,7 @@
 'use strict';
 
-var sketch = require('../server/hexboard/sketch');
+var sketch = require('../server/hexboard/sketch')
+  ;
 
 sketch.postRandomImage('localhost:9000');
 // sketch.postRandomImage('keynote2015-hexboard.demo.router.default.local:1080');
